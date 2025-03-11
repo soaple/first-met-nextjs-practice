b9400676feebcd411d1ec3668d17c0c98ec57472
Inje Lee <inje@soaple.io>
2025-03-11 13:02:39 +0900

    Chapter04 practice updated.

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,5 +1,20 @@
+import type { Metadata } from 'next';
+
 interface PageProps {
     params: Promise<{ postId: string }>;
+}
+
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+    const { params } = props;
+    const { postId } = await params;
+
+    return {
+        title: `Title (Post ID: ${postId})`,
+        description: `Description (Post ID: ${postId})`,
+        openGraph: {
+            images: [`/api/og?title=${`Post ID: ${postId}`}`],
+        },
+    };
 }
 
 async function PostItemPage(props: PageProps) {
