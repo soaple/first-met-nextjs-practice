9c52b775550b77af9a7fad363d522afcf617d3bd
Inje Lee <inje@soaple.io>
2025-03-11 23:36:05 +0900

    Chapter10 practice updated.

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,9 +1,14 @@
 import { Suspense } from 'react';
 import PostList from '@/components/PostList';
-import { readPostsAction } from '@/lib/actions/posts';
+import {
+    readPostsAction,
+    readPostsActionWithCache,
+    resetCache,
+} from '@/lib/actions/posts';
 
 function FeedPage() {
     const postsPromise = readPostsAction();
+    // const postsPromise = readPostsActionWithCache();
 
     return (
         <div className='box page'>
@@ -12,6 +17,8 @@ function FeedPage() {
             <Suspense fallback={<div>Loading...</div>}>
                 <PostList postsPromise={postsPromise} />
             </Suspense>
+
+            <button onClick={resetCache}>Reset Cache</button>
         </div>
     );
 }
--- lib/actions/posts.ts
+++ lib/actions/posts.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import samplePosts from '@/lib/constants/sample-posts.json';
 
 export async function readPostsAction(): Promise<MyNextApp.Post[]> {
@@ -8,4 +9,14 @@ export async function readPostsAction(): Promise<MyNextApp.Post[]> {
             resolve(samplePosts);
         }, 2000);
     });
+}
+
+export async function readPostsActionWithCache(): Promise<MyNextApp.Post[]> {
+    return fetch('http://localhost:3000/api/posts', {
+        cache: 'force-cache',
+    }).then((response) => response.json());
+}
+
+export async function resetCache() {
+    revalidatePath('/feed', 'page');
 }
