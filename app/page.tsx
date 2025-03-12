467c71a0f0b40fde8f954dcccaf2856273171a0a
Inje Lee <inje@soaple.io>
2025-03-12 12:54:12 +0900

    Chapter14 practice updated.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ console.log(process.env.SERVER_ENV_VAR);
 console.log(process.env.NEXT_PUBLIC_CLIENT_ENV_VAR);
 
 function IndexPage() {
-    return <div className='box page'>IndexPage</div>;
+    return (
+        <div className='box page'>
+            <h1>IndexPage</h1>
+        </div>
+    );
 }
 
 export default IndexPage;
